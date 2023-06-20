9fe331b7baf5808310e88bd5a13cc9db6111032d
artbodrov <artem.bodrov@gmail.com>
2023-06-20 23:46:06 +0300

    add Cli class

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public  class Cli {
+public static String inuptName(){
+    Scanner scanner = new Scanner(System.in);
+
+    return scanner.nextLine();
+}
+}
