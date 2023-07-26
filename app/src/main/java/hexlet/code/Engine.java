b6b8325d4484dcba673c8e4a40fdbc5750cdf6ee
artbodrov <artem.bodrov@gmail.com>
2023-07-26 14:55:32 +0300

    refactor 'run' method

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -14,7 +14,7 @@ public class Engine {
         System.out.println(ruleOfGame);
     }
 
-    public static void run(String ruleOfGame, String[][] arr) {
+    public static void runOld(String ruleOfGame, String[][] arr) {
 
         System.out.print("May I have your name? ");
         String name = Cli.inuptName();
@@ -52,6 +52,43 @@ public class Engine {
                 System.out.println(strings[question]);
                 isCorrect = compareAnswers(strings[answer], name);
 
+            } else {
+                break;
+            }
+        }
+        if (isCorrect) {
+            System.out.println("Congratulations, " + name + "!");
+        }
+    }
+
+    public static void run(String ruleOfGame, String[][] arr) {
+
+        boolean isCorrect = true;
+        int question = 0;
+        int answer = 1;
+        String yourAnswer;
+
+        System.out.print("May I have your name? ");
+        String name = Cli.inuptName();
+        System.out.println("Hello, " + name + "!");
+        System.out.println(ruleOfGame);
+
+        for (String[] strings : arr) {
+            if (isCorrect) {
+                System.out.print("Question: ");
+                System.out.println(strings[question]);
+                Scanner scanner = new Scanner(System.in);
+                yourAnswer = scanner.nextLine();
+                System.out.println("Your answer: " + yourAnswer);
+                if (yourAnswer.equals(String.valueOf(strings[answer]))) {
+                    System.out.println("Correct!");
+                } else {
+                    isCorrect = false;
+                    System.out.println("'"
+                            + yourAnswer + "'" + " is wrong answer ;(. Correct answer was " + "'"
+                            + answer + "'" + ". Let's try again, "
+                            + name + "!");
+                }
             } else {
                 break;
             }
